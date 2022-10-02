dd3eeb58680667851b8e86a28a5a0b682410c4d3
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2022-10-02 19:12:51 +0200

    Replace pow(x, 2) by multiplications 'cause it's more efficient

--- src/vector/vector_2.c
+++ src/vector/vector_2.c
@@ -6,7 +6,7 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 11:12:29 by ycornamu          #+#    #+#             */
-/*   Updated: 2022/09/06 15:33:13 by ycornamu         ###   ########.fr       */
+/*   Updated: 2022/10/02 17:04:16 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ t_vector	vec_div(const t_vector vec, const double nb)
 
 double	vec_length_squared(const t_vector vec)
 {
-	return (pow(vec.x, 2) + pow(vec.y, 2) + pow(vec.z, 2));
+	return (vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
 }
 
 double	vec_length(const t_vector vec)
