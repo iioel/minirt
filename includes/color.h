42397fda0f223c9aa644770b1e86db6937027c6d
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2022-10-04 18:44:04 +0200

    Cylinder v2

--- includes/color.h
+++ includes/color.h
@@ -6,7 +6,7 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 14:49:01 by ycornamu          #+#    #+#             */
-/*   Updated: 2022/10/04 00:22:20 by yoel             ###   ########.fr       */
+/*   Updated: 2022/10/04 15:50:38 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ typedef struct s_color
 	int	b;
 }				t_color;
 
-t_color	color_init(char r, char g, char b);
+t_color	color_init(int r, int g, int b);
+t_color	color_cap(t_color c1);
 t_color	color_add(t_color c1, t_color c2);
 t_color	color_max(t_color c1, t_color c2);
 int		color2int(t_color c);
--- src/color.c
+++ src/color.c
@@ -6,13 +6,13 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:53:37 by ycornamu          #+#    #+#             */
-/*   Updated: 2022/10/03 23:31:17 by ycornamu         ###   ########.fr       */
+/*   Updated: 2022/10/04 15:50:16 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "color.h"
 
-t_color	color_init(char r, char g, char b)
+t_color	color_init(int r, int g, int b)
 {
 	t_color	c;
 
@@ -22,17 +22,23 @@ t_color	color_init(char r, char g, char b)
 	return (c);
 }
 
-t_color	color_add(t_color c1, t_color c2)
+t_color	color_cap(t_color c1)
 {
-	c1.r = c1.r + c2.r;
-	c1.g = c1.g + c2.g;
-	c1.b = c1.b + c2.b;
 	if (c1.r > 255)
 		c1.r = 255;
 	if (c1.g > 255)
 		c1.g = 255;
 	if (c1.b > 255)
 		c1.b = 255;
+	return (c1);
+}
+
+t_color	color_add(t_color c1, t_color c2)
+{
+	c1.r = c1.r + c2.r;
+	c1.g = c1.g + c2.g;
+	c1.b = c1.b + c2.b;
+	c1 = color_cap(c1);
 	return (c1);
 }
 
