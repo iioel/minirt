2bc51144e27cb39606591b6f387f33b41ca50da0
yalthaus <yves.althaus42@gmail.com>
2022-10-13 18:06:04 +0200

    color ++ wip

--- src/color.c
+++ src/color.c
@@ -51,3 +51,17 @@ int	color2int(t_color c)
 {
 	return ((c.r << 16) + (c.g << 8) + c.b);
 }
+
+void	int2color(int n, t_color *c)
+{
+	c->r = (n & 0xff0000) >> 16;
+	c->g = (n & 0x00ff00) >> 8;
+	c->b = (n & 0x0000ff);
+}
+
+void	color_moy(t_color *result, t_color *c1, t_color *c2, int mult)
+{
+	result->r = ((c1->r * mult) + c2->r) / mult + 1;
+	result->g = ((c1->g * mult) + c2->g) / mult + 1;
+	result->b = ((c1->b * mult) + c2->b) / mult + 1;
+}
