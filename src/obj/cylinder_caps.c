c279182908c6a236ab6c193a011fdf62f37e32c6
Yoël Cornamusaz <iioel64@gmail.com>
2022-10-23 16:30:12 +0200

    Optimize cylinder calculations

--- src/obj/cylinder_caps.c
+++ src/obj/cylinder_caps.c
@@ -6,7 +6,7 @@
 /*   By: yoel <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:45:23 by yoel              #+#    #+#             */
-/*   Updated: 2022/10/18 20:06:35 by yoel             ###   ########.fr       */
+/*   Updated: 2022/10/23 16:27:16 by yoel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ double	cylinder_caps_inter(t_cylinder *c, t_ray *r, t_vector *n)
 	double	d2_len;
 	double	d_len_max;
 
-	d_len_max = sqrt((c->height / 2.) * (c->height / 2.)
-			+ (c->diameter / 2.) * (c->diameter / 2.));
+	d_len_max = c->half_sqrt;
 	t1 = cylinder_caps_inter_2(c, r, 1.);
 	t2 = cylinder_caps_inter_2(c, r, -1.);
 	d1_len = vec_length(vec_sub(vec_add(r->origin, vec_mul_nb(r->dir, t1)),
@@ -48,7 +47,7 @@ double	cylinder_caps_inter_2(t_cylinder *c, t_ray *r, int up_down)
 	t_vector	e;
 	t_vector	tmp;
 
-	tmp = vec_add(c->point, vec_mul_nb(c->vect, up_down * c->height / 2));
+	tmp = vec_add(c->point, vec_mul_nb(c->vect, up_down * c->height_half));
 	d = vec_sub(vec_mul(c->vect, tmp), vec_mul(c->vect, r->origin));
 	e = vec_mul(c->vect, r->dir);
 	t = (d.x + d.y + d.z) / (e.x + e.y + e.z);
